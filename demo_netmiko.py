f5a9626d7a73d01b7b39ed0e94cb348551e3167c
Trong_Chien VnPro <129259654+VnPro01@users.noreply.github.com>
2023-04-11 17:31:37 +0700

    Add files via upload

---
+++ demo_netmiko.py
@@ -0,0 +1,21 @@
+from netmiko import ConnectHandler
+
+sw={
+    "device_type":"cisco_ios",
+    "ip":"10.215.27.31",
+    "username":"vnpro",
+    "password":"vnpro#123",
+    "secret":"vnpro#321"
+}
+net_connect= ConnectHandler(**sw)
+net_connect.enable()
+"""data= net_connect.send_command('show int')
+print(data)"""
+
+for n in range(10,31):
+        CreateVlan=['vlan '+str(n)]
+        ipvlan= ['int vlan '+ str(n),'ip add 172.16.'+str(n)+".1 255.255.255.0","no shutdown"]
+        output=net_connect.send_config_set(CreateVlan)
+        output=net_connect.send_config_set(ipvlan)
+data= net_connect.send_command("show ip int brief | i Vlan")
+print(data)
---
+++ demo_showrouting.py
@@ -0,0 +1,23 @@
+from netmiko import ConnectHandler
+
+router={
+    "device_type":"cisco_ios",
+    "ip":"10.215.26.219",
+    "username":"vnpro",
+    "password":"vnpro#123",
+    "secret":"vnpro#321"
+}
+net_connect= ConnectHandler(**router)
+net_connect.enable()
+"""data= net_connect.send_command('show ip vrf int ')
+print(data)"""
+data = net_connect.send_command('show vrf brief | i ipv')
+for vrflines in data.splitlines():
+    vrfsplit=vrflines.split()
+    
+    if "4" in vrfsplit[3]:
+        vrfipv4= net_connect.send_command("show ip route vrf "+ vrfsplit[0])
+        print(vrfipv4)
+
+"""data = net_connect.send_command("show ip route vrf Customer_A")
+print(data)"""
